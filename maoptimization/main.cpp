b62e8c52301c9ddf1439aee3881c209d6fe32ca5
zlatula <zlatula@ZLATULYA>
2025-12-03 16:33:40 +0400

    new

--- maoptimization/main.cpp
+++ maoptimization/main.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <random>
-#include <algorithm>
+#include <ctime>
+using namespace std;
 
-std::vector<int> generatePrimesInRange(int min, int max) {
-    std::vector<bool> isPrime(max + 1, true);
-    isPrime[0] = isPrime[1] = false;
+bool isPrime(int n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2)
+        if (n % i == 0) return false;
+    return true;
+}
 
-    for (int i = 2; i * i <= max; i++) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= max; j += i)
-                isPrime[j] = false;
-        }
-    }
-
-    std::vector<int> primes;
-    for (int i = std::max(2, min); i <= max; i++) {
-        if (isPrime[i])
-            primes.push_back(i);
+vector<int> primesArray(int min, int max, int count) {
+    vector<int> primes;
+    srand((unsigned)time(0));
+    while (primes.size() < count) {
+        int value = min + rand() % (max - min + 1);
+        if (isPrime(value))
+            primes.push_back(value);
     }
     return primes;
 }
 
-std::vector<int> randomPrimes(int min, int max, int count) {
-    auto primes = generatePrimesInRange(min, max);
-    std::vector<int> result;
-    std::mt19937 gen(0); // фиксируем seed
-    std::uniform_int_distribution<int> dist(0, primes.size() - 1);
-
-    for (int i = 0; i < count; i++) {
-        result.push_back(primes[dist(gen)]);
-    }
-    return result;
-}
-
 int main() {
-    auto start = clock();
-    auto primes = randomPrimes(8000000, 9000000, 10000);
-    auto duration = clock() - start;
-
-    std::cout << "Primes count: " << primes.size()
+    clock_t start = clock();
+    auto primes = primesArray(8000000, 9000000, 1000); 
+    clock_t duration = clock() - start;
+    cout << "Primes count: " << primes.size()
         << " duration: " << duration << " ticks\n";
 }
