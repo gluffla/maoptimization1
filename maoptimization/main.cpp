84e144b27e44c7ec6a5b30d9f653f721a861f481
zlatula <zlatula@ZLATULYA>
2025-12-03 16:52:49 +0400

    g

--- maoptimization/main.cpp
+++ maoptimization/main.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <time.h>
 using namespace std;
 
-bool isPrime(int n) {
+
+bool isPrimeBase(int n)
+{
     if (n < 2) return false;
     if (n == 2) return true;
-    if (n % 2 == 0) return false;
-    for (int i = 3; i * i <= n; i += 2)
-        if (n % i == 0) return false;
+        if (n % 2 == 0) return false;
+            int p = sqrt(n);
+            for (int i = 3; i <= p; i += 2)
+            {
+                if (n % 2 == 0) return false;
+            }
     return true;
 }
 
-vector<int> primesArray(int min, int max, int count) {
-    vector<int> primes;
-    srand((unsigned)time(0));
-    while (primes.size() < count) {
-        int value = min + rand() % (max - min + 1);
-        if (isPrime(value))
+std::vector<int> primesArray(int min, int max, int count)
+{
+    std::vector<int> primes;
+    srand(0);
+    for (int i = 0; i < count; i++)
+    {
+        int value = (rand() / 32767.0 * (max - min) + min);
+        if (isPrimeBase(value))
             primes.push_back(value);
     }
     return primes;
 }
 
-int main() {
-    clock_t start = clock();
-    auto primes = primesArray(8000000, 9000000, 1000); 
-    clock_t duration = clock() - start;
-    cout << "Primes count: " << primes.size()
-        << " duration: " << duration << " ticks\n";
+int main()
+{
+    auto start = clock();
+    auto result = primesArray(8000000, 9000000, 10000);
+    auto duration = clock() - start;
+    std::cout << "Primes count: " << result.size() << " duration: " << duration << " msec.\n";
 }
